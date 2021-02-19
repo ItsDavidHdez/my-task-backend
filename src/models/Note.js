63b54d322fecbbbfaea8b50f13be47554144e719
David Vargas Hernandez <david-0610@live.com.mx>
2021-02-19 02:06:27 -0600

    Update note schema

--- src/models/Note.js
+++ src/models/Note.js
@@ -14,6 +14,10 @@ const noteSchema = new Schema(
       type: String,
       required: true,
     },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
   {
     timestamps: true,
